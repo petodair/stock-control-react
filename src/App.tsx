437fc26816213937b72113ac32340aa6e3dd956b
Peterson Odair Souza Batista <petersomodair@gmail.com>
2026-07-31 01:13:55 -0300

    feat: shadcn adicionado ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <h1 className="text-4xl text-amber-700">Hello word!</h1>;
+  return (
+    <>
+      <h1 className="text-4xl text-amber-700">Hello word!</h1>
+      <Button>Teste</Button>
+    </>
+  );
 }
 
 export default App;
